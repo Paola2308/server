c9ae5a5d45b401cc0875384c5cec42df44135400
Paola2308 <59588660+Paola2308@users.noreply.github.com>
2020-02-11 15:01:32 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ try:
 except:
 	from urllib.parse import urlparse, parse_qs
 
-import os
+import os #asigna un puerto manualmente 
 port = int(os.environ.get("PORT", 5000))	
 PORT_NUMBER = port
 
@@ -23,12 +23,12 @@ print('led off')
 
 #This class will handles any incoming request from
 #the browser 
-class myHandler(BaseHTTPRequestHandler):
+class myHandler(BaseHTTPRequestHandler): #existe metodos get y post  y el get se dispara cuando se abre la pagina web
 	
 	#Handler for the GET requests
 	def do_GET(self):
     
-		path=self.path
+		path=self.path                       #path es parte de la direccion es el / despues de url
 		print(self.path.split('/')[-1])
 		nombre=self.path.split('/')[-1]
 		datos=''
@@ -44,9 +44,9 @@ class myHandler(BaseHTTPRequestHandler):
 			#Check the file extension required and
 			#set the right mime type
 
-			sendReply = False
-			if nombre.endswith(".html"):
-				mimetype='text/html'
+			sendReply = False                        #especifica el tipo de respuesta da un mensaje al servidor 
+			if nombre.endswith(".html"):                 #cuando termina con html es que es tipo texto
+				mimetype='text/html'                      #tipo texto en formato html
 				f=open(nombre)
 				datos=f.read()
 				f.close()
@@ -57,7 +57,7 @@ class myHandler(BaseHTTPRequestHandler):
 			if self.path.endswith(".gif"):
 				mimetype='image/gif'
 				sendReply = True
-			if self.path.endswith(".js"):
+			if self.path.endswith(".js"):                       
 				mimetype='application/javascript'
 				sendReply = True
 			if self.path.endswith(".css"):
@@ -67,14 +67,14 @@ class myHandler(BaseHTTPRequestHandler):
 			if sendReply == True:
 				#Open the static file requested and send it
 				#f = open(curdir + sep + self.path,'r') 
-				self.send_response(200)
+				self.send_response(200)                  # el codigo 200 es una respuesta exitosa
 				self.send_header('Content-type',mimetype)
 				self.end_headers()
 				
 				try:
-					self.wfile.write(datos)
+					self.wfile.write(datos)                  #envia respuesta dependiendo si voy a trabajoar en python 2 o 3
 				except:
-					self.wfile.write(bytes(datos, 'UTF-8'))
+					self.wfile.write(bytes(datos, 'UTF-8'))   #paython 3
 				
 			return
 
@@ -85,7 +85,7 @@ class myHandler(BaseHTTPRequestHandler):
 try:
 	#Create a web server and define the handler to manage the
 	#incoming request
-	server = HTTPServer(('0.0.0.0', PORT_NUMBER), myHandler)
+	server = HTTPServer(('0.0.0.0', PORT_NUMBER), myHandler)           #cuando especifique la direccion recibe peticiones de cualaquuier ip y 0.0.0. es acceder al computador
 	print ('Started httpserver on port ' , PORT_NUMBER)
 	
 	#Wait forever for incoming htto requests
